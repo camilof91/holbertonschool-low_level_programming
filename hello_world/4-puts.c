e2f3b8a5886868625695ad11874a71bc651b2058
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-26 14:52:49 -0700

    add script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
- * main - prints "Programming is like building a multilingual puzzle
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); 
+puts( "\"Programming is like building a multilingual puzzle" ); 
 return(0);
 }
