a5a4460cb4c0b63d86a181a52a9fcfd0d9f4acf6
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-30 11:43:00 -0700

    modify script 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  *In the main function, an integer random number is generated,
  *and it is checked that if it is negative, it is converted
  *to a positive number. The last digit is calculated by
- *taking the modulo 10. If the number is zero, the 
+ *taking the modulo 10. If the number is zero, the
  *result is printed directly without entering the loop.
  *
  *
@@ -22,15 +22,15 @@ num = n;
 aux = num % 10;
 if (aux < 6 && aux != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n , aux);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, aux);
 }
-else if(aux > 5)
+else if (aux > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n , aux);
+printf("Last digit of %d is %d and is greater than 5\n", n, aux);
 }
-else if(aux == 0)
+else if (aux == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n , aux);
+printf("Last digit of %d is %d and is 0\n", n, aux);
 }
 return (0);
 }
