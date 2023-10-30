e1fb07856a95791c6f62145e566829b19f9bbfbd
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-30 15:40:06 -0700

    add script 1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+/*This program prints _putchar*/
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: This is the main function that prints _putchar
+*Return: Always returns 0 for successful execution.
+*
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
