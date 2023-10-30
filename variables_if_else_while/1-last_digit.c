472c1f1f74a0750ab1c8ac968d054ead37ac8d73
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-30 11:40:07 -0700

    modify script 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* 
+/*
  *the funtion main
- *In the main function, an integer random number is generated, 
- *and it is checked that if it is negative, it is converted 
- *to a positive number. The last digit is calculated by 
+ *In the main function, an integer random number is generated,
+ *and it is checked that if it is negative, it is converted
+ *to a positive number. The last digit is calculated by
  *taking the modulo 10. If the number is zero, the 
  *result is printed directly without entering the loop.
  *
@@ -13,24 +13,24 @@
 */
 int main(void)
 {
-  int n;
-  int num = 0;
-  int aux = 0;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  num = n;
-  aux = num % 10;
-  if (aux < 6 && aux != 0)
-    {
-      printf("Last digit of %d is %d and is less than 6 and not 0\n", n , aux);
-    }
-  else if(aux > 5)
-    {
-      printf("Last digit of %d is %d and is greater than 5\n", n , aux);
-    }
-  else if(aux == 0)
-    {
-      printf("Last digit of %d is %d and is 0\n", n , aux);
-    }
-    return (0);
+int n;
+int num = 0;
+int aux = 0;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+num = n;
+aux = num % 10;
+if (aux < 6 && aux != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n , aux);
+}
+else if(aux > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n , aux);
+}
+else if(aux == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n , aux);
+}
+return (0);
 }
