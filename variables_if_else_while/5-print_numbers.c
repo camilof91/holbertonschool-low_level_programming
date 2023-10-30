e19181c22684e27f0fed4cd37daf0db6ef2aa501
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-30 13:52:43 -0700

    modify script 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/*This program prints the numbers from 0 to 9*/
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: This is the main function that prints the firts ten numbers from 0 to 9.
+*Return: Always returns 0 for successful execution.
+*
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d",num);
+}
+printf("\n");
+return (0);
+}
