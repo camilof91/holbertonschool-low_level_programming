129aa55133dbb251afce748d19f8a74cdaa4082b
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-30 14:27:46 -0700

    add script 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+/*This program prints the numbers of base 16*/
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: This is the main function that prints the numbers.
+*of base 16 using putchar
+*Return: Always returns 0 for successful execution.
+*
+*/
+int main(void)
+{
+int num;
+for (num = 1; num < 16; num++)
+{
+if (num < 10)
+{
+putchar(num + '0');
+}
+else
+{
+putchar(num - 10 + 'A');
+}
+}
+putchar('\n');
+return (0);
+}
