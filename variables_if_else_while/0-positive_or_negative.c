b6f097bf41360012c28d94d6f69a656a6bce84c6
CamiloFetecua <camilofetecua2011@gmail.com>
2023-10-30 08:47:42 -0700

    modify script 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/*Zero, positive,negative*/
+
 /* 
  *betty style doc for function main goes there
  *This program shows you whether the number in 'n'
@@ -8,20 +10,20 @@
 */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n==0)
-    {
-      printf("%d is zero\n",n);
-    }
-  else if (n<0)
-    {
-      printf("%d is negative\n",n);
-    }
-  else if (n>0)
-    {
-      printf("%d is positive\n",n);
-    }
-  return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n==0)
+{
+printf("%d is zero\n",n);
+}
+else if (n<0)
+{
+printf("%d is negative\n",n);
+}
+else
+{
+printf("%d is positive\n",n);
+}
+return (0);
 }
