62f7779f2bdaa3ca0eba7ef246381203fbe237b0
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-01 06:12:07 -0700

    add script 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/*This program prints the alphabet 10 times in lowercase letters.*/
+#include "main.h"
+/**
+*print_alphabet_x10 - Entry point
+*
+*Description: This is the main function that prints the alphabet.
+*Return: void
+*
+*/
+void print_alphabet_x10(void)
+{
+char a;
+int c;
+for (c = 0; c < 10; c++)
+{
+for (a = 'a';a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
