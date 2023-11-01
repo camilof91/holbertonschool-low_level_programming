e0b51d3a9d1553162d1cf29d2507dcd59a5e3069
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-01 16:25:23 -0700

    modify script 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,13 +10,15 @@
 void jack_bauer(void)
 {
 int hd, hu, md, mu;
-for (hd = 0;hd <= 2;hd++)
+for (hd = 0; hd <= 2; hd++)
 {
-for (hu = 0; hu <= 3; hu++)
+for (hu = 0; hu <= 9; hu++)
+{
+if ((hd <= 1 && hu <= 9)|| (hd <= 2 && hu <= 3))
 {
 for (md = 0; md <= 5; md++)
 {
-for(mu = 0; mu <= 9; mu++)
+for (mu = 0; mu <= 9; mu++)
 {
 _putchar(hd + '0');
 _putchar(hu + '0');
@@ -24,6 +26,7 @@ _putchar(58);
 _putchar(md + '0');
 _putchar(mu + '0');
 _putchar('\n');
+}
 }
 }
 }
