8f21b3bf4554a7c13aae7b4bec42ba25fd7f15b5
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-01 16:16:13 -0700

    add script 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+/*This program prints 24 horas.*/
+#include "main.h"
+/**
+ *jack_bauer - Entry point
+ *
+ *Description: This is the main function that prints 24 horas.
+ *Return: void.
+ *
+ */
+void jack_bauer(void)
+{
+int hd, hu, md, mu;
+for (hd = 0;hd <= 2;hd++)
+{
+for (hu = 0; hu <= 3; hu++)
+{
+for (md = 0; md <= 5; md++)
+{
+for(mu = 0; mu <= 9; mu++)
+{
+_putchar(hd + '0');
+_putchar(hu + '0');
+_putchar(58);
+_putchar(md + '0');
+_putchar(mu + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
