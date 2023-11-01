d89b1b2729848d7d3cf045318b928e21985fffa5
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-01 16:58:59 -0700

    add script 9

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/*This program prints the alphabet in lowercase letters.*/
+#include "main.h"
+/**
+ *add - Entry point
+ *
+ *Description: This is the main function that prints the alphabet.
+ *Return: Always returns two ints.
+ *
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
