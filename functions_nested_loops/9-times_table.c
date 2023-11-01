a984c8f01830c663e2fd0f7dc5729f292a740912
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-01 16:46:10 -0700

    modify script 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+/*This program prints a char.*/
+#include "main.h"
+/**
+*times_table - Entry point
+*
+*Description: This is the main function that prints the alphabet.
+*Return: void
+*
+*/
+void times_table(void)
+{
+int a, b, c, d, e;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c > 9)
+{
+d = a % 10;
+e = (c - d) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(e + '0');
+_putchar(d + '0');
+}
+else
+{
+if (b != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(c + '0');
+}
+}
+_putchar('\n');
+}
+}
