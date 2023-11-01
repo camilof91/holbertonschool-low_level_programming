e126332d7400002acfd4fa46951d60dcd385e81c
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-01 06:25:14 -0700

    modify script 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+/*This program identify if there are caracter is in lowercase .*/
+#include "main.h"
+/**
+ *_islower - Entry point
+ *
+ *Description: This is the main function that prints the alphabet.
+ *Return: 1 for lowercase and 0 for other kind of caracter
+ *
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
