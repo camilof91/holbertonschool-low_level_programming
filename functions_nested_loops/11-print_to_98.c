01e76c6841c17655ade52b2b2a0c6b93b3bd1427
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-02 06:13:02 -0700

    modify script 10

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+/*This program prints the number until 98.*/
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - Entry point
+ *@n: number to star
+ *Description: This is the main function that prints the numbers
+ *form n to 98
+ *Return: Always returns 0 for successful execution.
+ *
+ */
+void print_to_98(int n)
+{
+int num = n;
+if (num < 98)
+{
+for (; num <= 98; num++)
+{
+printf("%d", num);
+if (num != 98)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+}
+else if (num > 98)
+{
+for (; num <= 98; num--)
+{
+printf("%d", num);
+if (num != 98)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+}
+else
+{
+printf("%d\n", num);
+}
+}
