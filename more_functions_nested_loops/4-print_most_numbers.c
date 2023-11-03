79e9f229e67645cc5ace68fe40aab75bc9e7f642
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-02 18:07:40 -0700

    add script 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+/*This program prints numbers.*/
+#include "main.h"
+/**
+ *print_most_numbers - Entry point
+ *
+ *Description: This is the main function that prints numbers.
+ *Return: Always returns 0 for successful execution.
+ *
+ */
+void print_most_numbers(void)
+{
+int num;
+for (num = 0; num < 10; num ++)
+{
+if (num != 2 && num != 4)
+{
+_putchar(num + '0');
+}
+}
+_putchar('\n');
+}
