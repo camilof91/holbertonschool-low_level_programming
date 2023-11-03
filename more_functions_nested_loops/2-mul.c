75716f154ecc1eadb58b93362802b750037d10e0
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-02 17:44:08 -0700

    add script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/*This program multiplies tow numbers.*/
+#include "main.h"
+/**
+ *mul - Entry point
+ *
+ *Description: This is the main function multiplies a and b.
+ *@a:number
+ *@b:number
+ *Return: multiplication betwin a and b
+ *
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
