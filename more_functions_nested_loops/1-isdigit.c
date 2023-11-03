dd6471d4390d16cf53e8ab9010c9a90c1ca70aab
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-02 17:35:24 -0700

    add script 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/*This program veryfy if it a number o letters.*/
+#include <stdio.h>
+/**
+ *int _isdigit - Entry point
+ *
+ *Description: This is the main function that prints the alphabet.
+ *@c: number to verify
+ *Return: Always returns 0 for successful execution.
+ *
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
