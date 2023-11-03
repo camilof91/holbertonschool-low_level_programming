8300cd8af28fbfa35487422aa1ffbb98c66dfbce
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-02 18:37:03 -0700

    add script 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+/*This program prints aline*/
+#include "main.h"
+/**
+ *print_line - Entry point
+ *
+ *Description: This is the main function that prints a line.
+ *Return: Always returns 0 for successful execution.
+ *
+ */
+void print_line(int n)
+{
+int aux;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for(aux = 0; aux < n; aux++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
