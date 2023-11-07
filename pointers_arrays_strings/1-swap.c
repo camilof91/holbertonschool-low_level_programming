234fe53a795c0447c56b6666b349deede061d9b3
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-07 05:47:53 -0800

    Add funtion 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/*This program changes the value assigned between two variables.*/
+#include "main.h"
+/**
+ *swap_int - Entry point
+ *
+ *Description: This is the main function change the assigned value
+ *between two variables
+ *@a: pointer int &a
+ *@b: pointer int &b
+*Return: Always returns 0 for successful execution.
+ *e
+ */
+void swap_int(int *a, int *b)
+{
+int aux;
+*a = *b;
+*b = aux;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,9 +3,7 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
-
-
-
+void wap_int(int *a, int *b);
 
 
 
