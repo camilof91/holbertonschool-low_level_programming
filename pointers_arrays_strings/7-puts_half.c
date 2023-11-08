a891ae4e8b91ceb26ea5fe4ee5181b0a4e35fad8
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-08 04:07:22 -0800

    add funtion 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+/*This program print half of a string.*/
+#include "main.h"
+/**
+ *puts_half - Entry point
+ *
+ *Description: This is the main function that print half of a string.
+ *@str: that the string
+ *Return: Always returns 0 for successful execution.
+ *
+ */
+void puts_half(char *str)
+{
+int len = 0, i = 0;
+ while (str[len] != '\0')
+{
+len++;
+}
+i = len / 2;
+while (i < len)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
