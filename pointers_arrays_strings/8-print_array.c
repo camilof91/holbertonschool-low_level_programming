1020e7865dff061672d7f5f8a37842c2f4410943
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-08 04:31:31 -0800

    add funtion 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+/*This program prints print n elements of int.*/
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - Entry point
+ *
+ *Description: This is the main function print n elements of int.
+ *@a: pointer to int
+ *@n: int value 
+ *Return: Always returns 0 for successful execution.
+ *
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != (n-1))
+{
+printf (", ");
+}
+}
+printf("\n");
+}
