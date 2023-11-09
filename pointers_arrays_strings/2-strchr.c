0cd16c4d991e460f5b0e3068e7851cbc1c6769c7
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-09 10:07:27 -0800

    add 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/*This program locate a character in a string.*/
+#include "main.h"
+/**
+ *_strchr - Entry point
+ *
+ *Description: This is the main function locate a character in a string.
+ *@s: string
+ *@c: character
+ *Return: return s 
+ *
+ */
+char *_strchr(char *s, char c)
+{
+
+while (*s != '\n')
+{
+if (*s == c)
+{
+return (s);
+}
+++s;
+}
+return (0);
+}
