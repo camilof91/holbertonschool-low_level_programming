dab8b2d477f041d23d2b4650f5bea27fe19041cc
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-09 07:35:56 -0800

    add funtion 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/*This program fills memory with a bute.*/
+#include "main.h"
+/**
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = *(src + i);
+}
+return (dest);
+}
