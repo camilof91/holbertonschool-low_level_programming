94d13951e7c637c61a3f44f0f783614752ae13ca
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-10 04:11:07 -0800

    add 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-int i = 0, j = 0;
-while (i < 8)
+int i, j;
+for (i = 0; i < 8; i++)
 {
-while (j < 8)
+for (j = 0; j < 8; j++)
 {
 _putchar (a[i][j]);
-j++;
 }
 _putchar ('\n');
-i++;
 }
 }
