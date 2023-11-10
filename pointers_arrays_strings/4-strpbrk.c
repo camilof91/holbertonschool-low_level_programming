248afd58f73487eea7b86451b60e862899c75725
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-10 14:44:16 -0800

    add 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,16 +1,25 @@
-# include "main.h"
-
+#include "main.h"
+/**
+ * _strpbrk - matches any character specified
+ * @s: This is the C string to be scanned.
+ * @accept:character in str1 that matches one of the characters in str2
+ *Return: string s that matches any character specified in accept
+ **/
 char *_strpbrk(char *s, char *accept)
 {
-  int i;
-  
-  for (i = 0; *(s + i) != '\0';i++)
-    {
-      
-      if (*s == *(accept + i))
-	{
-	  return (s);
-	}
-    }
-  return (0);
+int j;
+while (*s != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (*s == accept[j])
+{
+return (s);
+}
+j++;
+}
+s++;
+}
+return (0);
 }
