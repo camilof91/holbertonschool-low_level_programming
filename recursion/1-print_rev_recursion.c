00d590d86ffb262f44883295032f177543046ea9
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-16 08:31:51 -0800

    add funtion1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+/*This program prints the alphabet in lowercase letters.*/
+#include "main.h"
+/**
+*main - Entry point
+*
+*Description: This is the main function that prints the alphabet.
+*Return: Always returns 0 for successful execution.
+*
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0') 
+{
+_print_rev_recursion(s + 1);
+}
+_putchar(*s);
+}
+
