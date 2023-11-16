2fc4249271dd3a98c9ad0634cd102a1881f0eebc
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-16 11:46:41 -0800

    add funtion 3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *factorial - factorial of a given number
+ *@n: pointer block of memory to fill
+ *Return: factorial
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else if (n == 1)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n-1));
+}
+}
+
