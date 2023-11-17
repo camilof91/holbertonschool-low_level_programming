8d0dacc2edccaa77c7a85c9431592ad449358851
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-17 10:49:02 -0800

    add program 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Punto de entrada del programa
+ *@argc: Número de argumentos pasados al programa
+ *@argv: Matriz de cadenas que contiene los argumentos
+ *Return: Siempre 0 (éxito)
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;  /* Esta línea es para silenciar la advertencia de parámetro no utilizado */
+printf("%d\n", argc - 1);
+return (0);
+}
