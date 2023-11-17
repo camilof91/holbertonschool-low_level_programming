20467455155cd88dd4ae7bb1d94a3d1f58adc83f
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-17 11:40:43 -0800

    Añadir solución para 4-add

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 /**
- * main - Punto de entrada del programa
- * @argc: Número de argumentos pasados al programa
- * @argv: Matriz de cadenas que contiene los argumentos
- *
- * Return: 0 si es exitoso, 1 si hay un error
+ *main - Punto de entrada del programa
+ *@argc: Número de argumentos pasados al programa
+ *@argv: Matriz de cadenas que contiene los argumentos
+ *Return: 0 si es exitoso, 1 si hay un error
  */
 int main(int argc, char *argv[])
 {
@@ -16,12 +15,9 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-/* Convertir los argumentos a enteros */
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-/* Calcular la multiplicación */
 result = num1 * num2;
-/* Imprimir el resultado */
 printf("%d\n", result);
 return (0);
 }
