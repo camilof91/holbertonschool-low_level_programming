16e89e695e1876d249a114fc3ffc2e4e3faee2ed
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-22 05:10:00 -0800

    add funtion 2-calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - asigna memoria para un array de nmemb elementos,
+ * cada uno de tamaño size bytes
+ * @nmemb: número de elementos en el array
+ * @size: tamaño de cada elemento en bytes
+ * Devuelve: puntero a la memoria asignada; NULL si la asignación falla
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+/* Declara una variable de tipo puntero para almacenar la memoria asignada*/
+unsigned int i;  
+/* Declara una variable para la iteración*/
+if (nmemb == 0 || size == 0)
+return (NULL);
+/* Verifica si nmemb o size es igual a cero y devuelve NULL en ese caso*/
+/* Asigna memoria para nmemb elementos, cada uno de size bytes*/
+p = malloc(nmemb * size);
+/* Verifica si la asignación de memoria fue exitosa*/
+if (p == NULL)
+return (NULL);
+/* Inicializa la memoria asignada a 0 */
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
+/* Devuelve el puntero a la memoria asignada e inicializada */
+return (p);
+}
