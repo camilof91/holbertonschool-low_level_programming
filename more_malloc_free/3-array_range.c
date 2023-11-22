795d89df498b89b96cb924511627ab73d56f0a54
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-22 05:40:30 -0800

    add funtion 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,17 +10,17 @@ int *array_range(int min, int max)
 {
 int *arr, i = 0, t = min;
 /* Declaración de variables */
-    if (min > max)
-        return (0);
+if (min > max)
+return (0);
 /*Verifica si el valor mínimo es mayor que*/
 /*  el valor máximo y devuelve NULL si es así*/
 arr = malloc((max - min + 1) * sizeof(int));
 /* Asigna memoria para el array de enteros */
-    if (arr == NULL)
-        return (0);
+if (arr == NULL)
+return (0);
 /* Verifica si la asignación de memoria fue exitosa*/
-    while (i <= max - min)
-        arr[i++] = t++;
+while (i <= max - min)
+arr[i++] = t++;
 /* Llena el array con valores desde min hasta max*/
 return (arr);
 /* Devuelve el puntero al array creado*/
