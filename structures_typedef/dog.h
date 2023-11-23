24e2db281823238e9ad7436b2168e789116206e0
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-23 10:02:50 -0800

    add a header dog with a structur

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog (string)
+ * @age: the age of the dog (float)
+ * @owner: the owner of the dog (string)
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+#endif /* DOG_H */
