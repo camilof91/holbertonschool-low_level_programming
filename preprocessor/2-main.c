cdf57a512633ff6361b02a780d00c2b51d61941a
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-23 08:34:11 -0800

    add funtion main

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - imprime el nombre de el programa del que fue compilado
- * Return 0
+ * Return: siempre 0
  */
 int main(void)
 {
-printf("%s\n",__FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
