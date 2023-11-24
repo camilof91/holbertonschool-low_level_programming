ff6f476f9e2bf8a3b3cdb2b056afa554a0ba00f8
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-24 12:12:15 -0800

    add

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -9,9 +9,8 @@
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+return (a + b);
 }
-
 /**
  * op_sub - returns the difference of a and b
  * @a: first integer
@@ -20,9 +19,8 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+return (a - b);
 }
-
 /**
  * op_mul - returns the product of a and b
  * @a: first integer
@@ -31,9 +29,8 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
-
 /**
  * op_div - returns the result of the division of a by b
  * @a: first integer
@@ -42,14 +39,13 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
-
+return (a / b);
+}
 /**
  * op_mod - returns the remainder of the division of a by b
  * @a: first integer
@@ -58,10 +54,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
