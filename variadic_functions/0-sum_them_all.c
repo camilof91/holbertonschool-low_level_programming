14e817f5657a7099ffcba764ccd6978b380474f4
CamiloFetecua <camilofetecua2011@gmail.com>
2023-11-30 09:11:03 -0800

    add funtion 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,15 +15,14 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 
-    va_start(args, n);
+	va_start(args, n);
 
-	for (i = 0; i < n; i++) 
+	for (i = 0; i < n; i++)
 	{
-        	int current_arg = va_arg(args, int);
+		int current_arg = va_arg(args, int);
 		sum += current_arg;
 	}
 
-    va_end(args);
-
-    return (sum);
+	va_end(args);
+	return (sum);
 }
