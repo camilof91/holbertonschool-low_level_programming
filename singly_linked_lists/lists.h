a8f423e92cc77552a2ef7db5713bd85802da415c
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-13 12:41:20 -0800

    Add the firts funtion

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,30 @@
-#ifndef LIST_H
-#define LSIT_H
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h);
 
-
 #endif
+
