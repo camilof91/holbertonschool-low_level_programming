2b8a003cc12fa0098cc4c0cb1a3d3335d0b1f5c0
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-13 13:32:06 -0800

    fix sintax mistakes

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,23 +8,24 @@
  * @str: string to be duplicated and added as the data for the new node
  * Return: the address of the new element, or NULL if it failed
  */
-lists_t *add_node(lists_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 	
 	lists_t *new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
+
 	return (new_node);
 }
