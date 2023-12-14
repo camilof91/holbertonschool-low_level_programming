9cdb8f7c7b6b15d901752d328b4520fbce67e129
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-14 05:18:48 -0800

    Add fix betty style

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,8 +3,8 @@
 
 /**
  * sum_dlistint - returns the number of elements in a linked dlistint_t list
- * @head - pointer to the head of the doubly linked list
- * Return - sum of all the data, or 0 if the list is empty
+ * @head: pointer to the head of the doubly linked list
+ * Return: sum of all the data, or 0 if the list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
