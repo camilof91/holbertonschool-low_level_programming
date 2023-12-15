48da0a7d914fc13a39699578c31ac703d37d049e
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-15 14:08:10 -0800

    Add program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,82 +6,84 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - Copia el contenido de un archivo a otro.
- * @argc: El número de argumentos en la línea de comandos.
- * @argv: Un array que contiene los argumentos de la línea de comandos.
- * Devuelve: 0 en caso de éxito, o el código de error correspondiente en caso de fallo.
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ * Return: 0 on success, or the corresponding error code on failure.
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
-	char buffer[BUFFER_SIZE];
+    int fd_from, fd_to, r, w;
+    char buffer[BUFFER_SIZE];
 
-	/* Verifica el número correcto de argumentos */
-	if (argc != 3)
-	{
-		write(STDERR_FILENO, "Uso: ", 6);
-		write(STDERR_FILENO, argv[0], 1); /* argv[0] es el nombre del programa*/
-		write(STDERR_FILENO, " archivo_desde archivo_hacia\n", 29);
-		exit(97);
-	}
+    /* Check for the correct number of arguments */
+    if (argc != 3)
+    {
+        write(STDERR_FILENO, "Usage: ", 7);
+        write(STDERR_FILENO, argv[0], 1); // argv[0] is the program name
+        write(STDERR_FILENO, " file_from file_to\n", 20);
+        exit(97);
+    }
 
-	/* Abre el archivo de origen en modo lectura */
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		write(STDERR_FILENO, "Error: No se puede leer del archivo ", 38);
-		write(STDERR_FILENO, argv[1], 1);
-		write(STDERR_FILENO, "\n", 1);
-		exit(98);
-	}
+    /* Open the source file in read-only mode */
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        write(STDERR_FILENO, "Error: Can't read from file ", 29);
+        write(STDERR_FILENO, argv[1], 1);
+        write(STDERR_FILENO, "\n", 1);
+        exit(98);
+    }
 
-	/* Abre o crea el archivo de destino en modo escritura, truncándolo si ya existe */
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		write(STDERR_FILENO, "Error: No se puede escribir en ", 32);
-		write(STDERR_FILENO, argv[2], 1);
-		write(STDERR_FILENO, "\n", 1);
-		exit(99);
-	}
+    /* Open or create the destination file in write-only mode, truncating it if it already exists */
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        write(STDERR_FILENO, "Error: Can't write to ", 23);
+        write(STDERR_FILENO, argv[2], 1);
+        write(STDERR_FILENO, "\n", 1);
+        exit(99);
+    }
 
-	/* Lee y escribe en bloques hasta que no quede nada por leer */
-	do
-	{
-		r = read(fd_from, buffer, BUFFER_SIZE);
-		if (r == -1)
-		{
-			write(STDERR_FILENO, "Error: No se puede leer del archivo ", 38);
-			write(STDERR_FILENO, argv[1], 1);
-			write(STDERR_FILENO, "\n", 1);
-			exit(98);
-		}
+    /* Read and write in blocks until nothing is left to read */
+    do
+    {
+        r = read(fd_from, buffer, BUFFER_SIZE);
+        if (r == -1)
+        {
+            write(STDERR_FILENO, "Error: Can't read from file ", 29);
+            write(STDERR_FILENO, argv[1], 1);
+            write(STDERR_FILENO, "\n", 1);
+            exit(98);
+        }
 
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			write(STDERR_FILENO, "Error: No se puede escribir en ", 32);
-			write(STDERR_FILENO, argv[2], 1);
-			write(STDERR_FILENO, "\n", 1);
-			exit(99);
-		}
-	} while (r == BUFFER_SIZE);
+        w = write(fd_to, buffer, r);
+        if (w == -1 || w != r)
+        {
+            write(STDERR_FILENO, "Error: Can't write to ", 23);
+            write(STDERR_FILENO, argv[2], 1);
+            write(STDERR_FILENO, "\n", 1);
+            exit(99);
+        }
 
-	/* Cierra los descriptores de archivo */
-	if (close(fd_from) == -1)
-	{
-		write(STDERR_FILENO, "Error: No se puede cerrar el descriptor ", 41);
-		write(STDERR_FILENO, "archivo_desde", 1);
-		write(STDERR_FILENO, "\n", 1);
-		exit(100);
-	}
+    } while (r == BUFFER_SIZE);
 
-	if (close(fd_to) == -1)
-	{
-		write(STDERR_FILENO, "Error: No se puede cerrar el descriptor ", 41);
-		write(STDERR_FILENO, "archivo_hacia", 1);
-		write(STDERR_FILENO, "\n", 1);
-		exit(100);
-	}
-	return (0);
+    /* Close the file descriptors */
+    if (close(fd_from) == -1)
+    {
+        write(STDERR_FILENO, "Error: Can't close fd ", 23);
+        write(STDERR_FILENO, "file_from", 1);
+        write(STDERR_FILENO, "\n", 1);
+        exit(100);
+    }
+
+    if (close(fd_to) == -1)
+    {
+        write(STDERR_FILENO, "Error: Can't close fd ", 23);
+        write(STDERR_FILENO, "file_to", 1);
+        write(STDERR_FILENO, "\n", 1);
+        exit(100);
+    }
+
+    return (0);
 }
