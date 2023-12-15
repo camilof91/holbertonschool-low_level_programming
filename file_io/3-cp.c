ac05cfafee24222bdf9f827dca01e32833cb0c16
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-15 15:33:58 -0800

    Add fix sintax mistakes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,6 +5,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+void print_error(const char *msg, const char *file)
+{
+    dprintf(2, "Error: %s %s\n", msg, file);
+}
+
 /**
  * main - Copies the content of a file to another file.
  * @ac: Number of arguments.
@@ -26,14 +31,14 @@ int main(int ac, char **av)
     fd_from = open(av[1], O_RDONLY);
     if (fd_from == -1)
     {
-        dprintf(2, "Error: Can't read from file %s\n", av[1]);
+        print_error("Can't read from", av[1]);
         exit(98);
     }
 
     fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
     {
-        dprintf(2, "Error: Can't write to %s\n", av[2]);
+        print_error("Can't write to", av[2]);
         close(fd_from);
         exit(99);
     }
@@ -43,7 +48,7 @@ int main(int ac, char **av)
         write_status = write(fd_to, buffer, read_status);
         if (write_status == -1)
         {
-            dprintf(2, "Error: Can't write to %s\n", av[2]);
+            print_error("Can't write to", av[2]);
             close(fd_from);
             close(fd_to);
             exit(99);
@@ -52,7 +57,7 @@ int main(int ac, char **av)
 
     if (read_status == -1)
     {
-        dprintf(2, "Error: Can't read from file %s\n", av[1]);
+        print_error("Can't read from", av[1]);
         close(fd_from);
         close(fd_to);
         exit(98);
@@ -60,13 +65,13 @@ int main(int ac, char **av)
 
     if (close(fd_from) == -1)
     {
-        dprintf(2, "Error: Can't close fd %d\n", fd_from);
+        print_error("Can't close fd", av[1]);
         exit(100);
     }
 
     if (close(fd_to) == -1)
     {
-        dprintf(2, "Error: Can't close fd %d\n", fd_to);
+        print_error("Can't close fd", av[2]);
         exit(100);
     }
 
