c1a52f44e5de4dc7c3ff152a27e8cdc6c4c4a8d5
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-15 15:08:31 -0800

    Add function that appends text at the end of a file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,80 +1,72 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
-#include <sys/stat.h>
 
-#define BUFFER_SIZE 1024
-
-void print_error(int code, const char *file_name) {
-    switch (code) {
-        case 97:
-            dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-            break;
-        case 98:
-            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
-            break;
-        case 99:
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
-            break;
-        case 100:
-            dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_name);
-            break;
-        default:
-            break;
-    }
-    exit(code);
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
-void copy_permissions(const char *source, const char *destination) {
-    struct stat st;
-    if (stat(source, &st) == 0) {
-        if (fchmod(destination, st.st_mode) == -1) {
-            print_error(99, destination);
-        }
-    }
-}
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-int main(int argc, char *argv[]) {
-    int fd_from, fd_to;
-    ssize_t read_bytes, write_bytes;
-    char buffer[BUFFER_SIZE];
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
-    if (argc != 3) {
-        print_error(97, NULL);
-    }
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1) {
-        print_error(98, argv[1]);
-    }
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if (fd_to == -1) {
-        close(fd_from);
-        print_error(99, argv[2]);
-    }
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
-        write_bytes = write(fd_to, buffer, read_bytes);
-        if (write_bytes != read_bytes) {
-            close(fd_from);
-            close(fd_to);
-            print_error(99, argv[2]);
-        }
-    }
-
-    if (read_bytes == -1) {
-        close(fd_from);
-        close(fd_to);
-        print_error(98, argv[1]);
-    }
-
-    if (close(fd_from) == -1 || close(fd_to) == -1) {
-        print_error(100, NULL);
-    }
-
-    copy_permissions(argv[1], argv[2]);
-
-    return 0;
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
