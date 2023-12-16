5fa24e72112f1b26c5efbcba60890c90961f67e9
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-15 16:18:34 -0800

    add function that prints the binary representation of a number.

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,19 +6,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1UL << 63; /* Assumes a 64-bit system */
+    if (n > 1)
+        print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (mask > 0)
-	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
-		mask >>= 1;
-	}
+    _putchar((n & 1) + '0');
 }
