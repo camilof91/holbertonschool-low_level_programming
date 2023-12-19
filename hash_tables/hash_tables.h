0103fc9b1ce72b5fc25969e9db2a294f3f767137
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-19 03:22:14 -0800

    fix sytax issues in hash_tables

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,4 @@
 #ifndef HASH_TABLES_H
-
 #define HASH_TABLES_H
 
 /**
@@ -34,7 +33,7 @@ typedef struct hash_table_s
 /* Function prototype */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);`
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
