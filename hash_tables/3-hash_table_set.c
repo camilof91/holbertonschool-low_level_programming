e11625b2393795c6f34ce34d24e0618314838377
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-22 10:35:14 -0800

    add a function that adds an element to the hash table.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,50 +3,23 @@
 #include <string.h>
 
 /**
- * hash_table_set - Adds an element to the hash table
- * @ht: The hash table to add or update the key/value to
- * @key: The key
- * @value: The value associated with the key
+ * create_node - Creates a new hash node.
+ * @key: The key string.
+ * @value: The value string.
  *
- * Return: 1 if it succeeded, 0 otherwise
- * In case of collision, add the new node at the beginning of the list
+ * Return: A pointer to the new node, or NULL on failure.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *create_node(const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current_node;
-
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	/* Check for collision */
-	current_node = ht->array[index];
-	while (current_node)
-	{
-		if (strcmp(current_node->key, key) == 0)
-		{
-			/* Update value if key already exists */
-			free(current_node->value);
-			current_node->value = strdup(value);
-			if (current_node->value == NULL)
-				return (0);
-			return (1);
-		}
-		current_node = current_node->next;
-	}
-
-	/* Create a new node */
-	new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (NULL);
 
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return (0);
+		return (NULL);
 	}
 
 	new_node->value = strdup(value);
@@ -54,11 +27,67 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(new_node->key);
 		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * insert_node_at_index - Inserts a node at a given index in the hash table.
+ * @ht: The hash table.
+ * @new_node: The node to insert.
+ * @index: The index at which to insert the node.
+ */
+void insert_node_at_index(hash_table_t *ht, hash_node_t *new_node, unsigned long int index)
+{
+	if (ht == NULL || new_node == NULL || index >= ht->size)
+		return;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+}
+
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	char *value_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new_node = create_node(key, value);
+	if (new_node == NULL)
+	{
+		free(value_copy);
 		return (0);
 	}
 
-	/* Handle collision by adding the new node at the beginning of the list */
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	insert_node_at_index(ht, new_node, index);
 	return (1);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -37,7 +37,13 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
-#endif
+/* New function prototypes */
+hash_node_t *create_node(const char *key, const char *value);
+void insert_node_at_index(hash_table_t *ht, hash_node_t *new_node, unsigned long int index);
+
+#endif /* HASH_TABLES_H */
 
