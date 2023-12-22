148bdd82096e0e7d278f7b37eb484187b09f72a1
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-22 09:48:18 -0800

    Add a function that adds an element to the hash table.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,25 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to the hash table.
- * @ht: The hash table to add or update the key/value to.
- * @key: The key (cannot be an empty string).
- * @value: The value associated with the key (must be duplicated).
+ * create_node - Creates a new hash node.
+ * @key: The key string.
+ * @value: The value string.
  *
- * Return: 1 if it succeeded, 0 otherwise.
+ * Return: A pointer to the new node, or NULL on failure.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *create_node(const char *key, const char *value)
 {
-    unsigned long int index = key_index((unsigned char *)key, ht->size);
     hash_node_t *new_node;
 
-    /* Create a new node with the given key and value */
-    new_node = create_node(key, value);
+    new_node = malloc(sizeof(hash_node_t));
     if (new_node == NULL)
-        return (0);
+        return (NULL);
 
-    /* Insert the new node at the beginning of the linked list at the calculated index */
-    insert_node_at_index(ht, new_node, index);
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-    return (1);
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->next = NULL;
+
+    return (new_node);
+}
+
+/**
+ * insert_node_at_index - Inserts a node at a given index in the hash table.
+ * @ht: The hash table.
+ * @new_node: The node to insert.
+ * @index: The index at which to insert the node.
+ */
+void insert_node_at_index(hash_table_t *ht, hash_node_t *new_node, unsigned long int index)
+{
+    if (ht == NULL || new_node == NULL || index >= ht->size)
+        return;
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
 }
