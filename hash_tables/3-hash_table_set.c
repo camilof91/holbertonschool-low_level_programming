36205c3ef478f57e7b095def8b3be7b8784afca4
CamiloFetecua <camilofetecua2011@gmail.com>
2023-12-22 09:37:47 -0800

    Add a function that adds an element to the hash table.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,3 +1,13 @@
+include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add or update the key/value to.
+ * @key: The key (cannot be an empty string).
+ * @value: The value associated with the key (must be duplicated).
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index = key_index((unsigned char *)key, ht->size);
